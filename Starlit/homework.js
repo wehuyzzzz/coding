e5c83f6bfdf836f8c7c448c4267510938f4f41b3
wehuyzzzz <lvhuy194@gmail.com>
2023-11-01 10:54:30 +0700

    loop exercise

--- Starlit/homework.js
+++ Starlit/homework.js
@@ -11,7 +11,7 @@ function BMI(a, b) {
 BMI(49, 50)
 
 function leapYear(a) {
-    if ((a % 4 === 0 && a % 100 !== 0 )|| a % 400 === 0) {
+    if ((a % 4 === 0 && a % 100 !== 0) || a % 400 === 0) {
         console.log(a, " là năm nhuận")
     }
     else {
@@ -30,24 +30,27 @@ leapYear(2400)
 // B4: code
 // B5: test
 
-function timx(a,b) {
-    if (a == 0 && b==0 ){
-        console.log ("x = infinitely many solutions ")
+function timx(a, b) {
+    if (a == 0 && b == 0) {
+        console.log("x = infinitely many solutions ")
     }
-    else if (b == 0 && a !== 0){
-        console.log ("x = null")
+
+    else if (b == 0 && a !== 0) {
+        console.log("x = null")
     }
 
     else {
-        console.log ("x = ",-b/a)
+        console.log("x = ", -b / a)
     }
 
 
 }
 
-timx(10,5)
-timx(0,0)
-timx(10,0)
+timx(10, 5)
+timx(0, 0)
+timx(10, 0)
+
+
 
 
 
---
+++ loop.js
@@ -0,0 +1,11 @@
+// bai 0: in ra cac so tu 1 - 10
+// bai 1: Viết hàm in ra số lẻ từ 1 - 10
+// bài 2: Viết hàm in ra số chẵn từ 1 - 10
+// bài 3: in ra tổng giá trị từ 1 - 10
+// bài 4: in ra tích giá trị 5 - 10
+
+
+function oddnumber(a) {
+let a = 0
+
+}
